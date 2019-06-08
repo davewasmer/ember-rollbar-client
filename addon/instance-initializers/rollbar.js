21403fd757dd8fd9df832d6ec6a6a17ed31a27aa
Dave Wasmer <davewasmer@gmail.com>
2019-06-07 17:16:01 -0700

    dont leak memory in fastboot

--- addon/instance-initializers/rollbar.js
+++ addon/instance-initializers/rollbar.js
@@ -1,24 +1,23 @@
 import Ember from 'ember';
 
 export function initialize(appInstance) {
-  let fastbootService = appInstance.lookup('service:fastboot');
-  let rollbarService = appInstance.lookup('service:rollbar');
-  let oldOnError = Ember.onerror || function() {};
+  if (typeof FastBoot === 'undefined') {
+    let rollbarService = appInstance.lookup('service:rollbar');
+    let oldOnError = Ember.onerror || function() {};
 
-  Ember.onerror = (...args) => {
-    oldOnError(...args);
-    let enabled = rollbarService.get('enabled');
+    Ember.onerror = (...args) => {
+      oldOnError(...args);
+      let enabled = rollbarService.get('enabled');
 
-    if (enabled) {
-      rollbarService.error(...args);
-    }
+      if (enabled) {
+        rollbarService.error(...args);
+      }
 
-    if (!enabled || Ember.testing) {
-      if (!fastbootService || !fastbootService.get('isFastBoot')) {
+      if (!enabled || Ember.testing) {
         throw args[0];
       }
-    }
-  };
+    };
+  }
 }
 
 export default { initialize };
